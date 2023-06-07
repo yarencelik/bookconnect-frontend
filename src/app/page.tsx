90edcf85df78832e5629a400974cd283b549db65
amr8644 <amr8644@gmail.com>
2023-06-07 17:51:13 +0300

    Add Chakra UI and Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+/* eslint-disable react/no-children-prop */
+// eslint-disable-next-line react/no-children-prop
 import Image from "next/image";
+import SidebarWithHeader from "./Navigation";
 
 export default function Home() {
-  return <main className=""></main>;
+   return (
+      <>
+         <SidebarWithHeader children={undefined} />
+      </>
+   );
 }
